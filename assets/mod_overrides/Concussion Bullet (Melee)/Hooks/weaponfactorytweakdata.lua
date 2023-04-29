972e72db3549a36d8d4fcc4e37a891b496eef0d4
Dr_Newbie <tf2baidonation@gmail.com>
2023-04-30 03:28:00 +0800

    .

--- assets/mod_overrides/Concussion Bullet (Melee)/Hooks/weaponfactorytweakdata.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(WeaponFactoryTweakData, "init", "InstantConcussionBulletMelee_TweakData", function(self)
-	for fac_id, fac_data in pairs(self) do
-		if tostring(fac_id):find("wpn_fps_clonefakewep_") then
-			table.insert(self[fac_id].uses_parts, "wpn_fps_concussionbullet_melee")
-		end
-	end
-end)
--- assets/mod_overrides/Melee2WEP/Hooks/tweakdata.lua
+++
@@ -1,1 +0,0 @@
-table.insert(tweak_data.gui.buy_weapon_categories.primaries, {"clonefakewep"})
